665d6edd50038cc36aedb26d08160c5458d5b11d
abhishekbankar002 <77184644+abhishekbankar002@users.noreply.github.com>
2022-01-04 15:42:31 +0530

    Add files via upload

---
+++ CropDetection.py
@@ -0,0 +1,112 @@
+import random
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.nn import functional as F
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+import config
+import customDataLoader
+import CNN
+
+train_loss_array = []
+
+
+def train(model, use_cuda, train_loader, optimizer, epoch):
+	model.train()
+
+	for batch_idx, (data, target) in enumerate(train_loader):
+
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+
+		optimizer.zero_grad()
+		output = model(data)
+
+		loss = F.cross_entropy(output, target)
+		train_loss_array.append(loss)
+		loss.backward()
+		optimizer.step()
+
+		if batch_idx % 100 == 0:
+			print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+				epoch, batch_idx * len(data), len(train_loader.dataset),
+				       100. * batch_idx / len(train_loader), loss.item()))
+
+
+accuracy = []
+
+
+def test(model, use_cuda, test_loader):
+	model.eval()
+
+	test_loss = 0
+	correct = 0
+
+	with torch.no_grad():
+		for data, target in test_loader:
+
+			if use_cuda:
+				data, target = data.cuda(), target.cuda()
+
+			output = model(data)
+
+			# test_loss += torch.sum((output - y_onehot) ** 2)
+			test_loss += F.cross_entropy(output, target, reduction='sum')
+			pred = output.argmax(dim=1, keepdim=True)
+
+			correct += pred.eq(target.view_as(pred)).sum().item()
+
+	test_loss /= len(test_loader.dataset)
+	accuracy.append(100. * correct / len(test_loader.dataset))
+	print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+		test_loss, correct, len(test_loader.dataset),
+		100. * correct / len(test_loader.dataset)))
+
+
+def seed(seed_value):
+	torch.cuda.manual_seed_all(seed_value)
+	torch.manual_seed(seed_value)
+	torch.cuda.manual_seed(seed_value)
+	np.random.seed(seed_value)
+	random.seed(seed_value)
+	torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+
+
+if __name__ == "__main__":
+
+	use_cuda = True
+	seed(config.seed)
+	train_loader = torch.utils.data.DataLoader(customDataLoader.customDatasetClass(config.path + 'train'),
+	                                           num_workers=config.numWorkers['train'], shuffle=True,
+	                                           batch_size=config.batchSize['train'])
+	test_loader = torch.utils.data.DataLoader(customDataLoader.customDatasetClass(config.path + 'test'),
+	                                          num_workers=config.numWorkers['test'],
+	                                          shuffle=False,
+	                                          batch_size=config.batchSize['test'])
+	model = CNN.Net()
+	if use_cuda:
+		model = model.cuda()
+
+	optimizer = optim.Adam(model.parameters(), lr=config.lr)
+
+	for epoch in range(1, config.epochs + 1):
+		train(model, use_cuda, train_loader, optimizer, epoch)
+		test(model, use_cuda, test_loader)
+
+	plt.title(' Train Loss')
+	plt.plot([j for j in range(len(train_loss_array))], train_loss_array)
+	plt.savefig('Train loss')
+	plt.show()
+
+	plt.title(' Accuracy')
+	plt.plot([j for j in range(len(accuracy))], accuracy)
+	plt.savefig('Accuracy')
+	plt.show()
+
+	torch.save(model.state_dict(), "cropDetection.pt")
+
+	# dummy_input = torch.rand(1, 1, 64, 64).cuda()
+	# torch.onnx.export(model, dummy_input, 'model.onnx', verbose=True, input_names=['data'], output_names=['output'])
+	# model.load_state_dict(torch.load('quickdraw.pt'))
